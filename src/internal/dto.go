82c8056d489ce2adfcc8d08df987128f39272b09
tince250 <t.s.mihajlovic25@gmail.com>
2025-10-27 23:05:21 +0100

    Added getting ratings with average per host or room

--- src/internal/dto.go
+++ src/internal/dto.go
@@ -1,7 +1,21 @@
 package internal
 
+import "time"
+
 type CreateRatingDTO struct {
 	TargetID    	uint         `json:"targetId"`
 	Score       	int          `json:"score"`
 	Comment     	string       `json:"comment"`
+}
+
+type RatingDTO struct {
+	Username string    `json:"username"`
+	Score    int       `json:"score"`
+	Comment  string    `json:"comment"`
+	Time     time.Time `json:"time"` 
+}
+
+type RatingsWithAverageDTO struct {
+	Average float64         `json:"average"`
+	Ratings []RatingDTO 	`json:"ratings"`
 }
