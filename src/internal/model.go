c734e30094bb2618674e21c94acf2f1ff0cf30f0
tince250 <t.s.mihajlovic25@gmail.com>
2025-10-27 20:24:38 +0100

    Added a unified rating model for hosts and rooms

--- src/internal/model.go
+++ src/internal/model.go
@@ -1,1 +1,19 @@
 package internal
+
+type RatingType string
+
+const (
+	Host  RatingType = "host"
+	Room RatingType = "room"
+)
+
+type Rating struct {
+	ID          	uint           `gorm:"primaryKey"`
+	TargetType  	RatingType     `gorm:"index:idx_target_rater,priority:1;index"`
+	TargetID    	string         `gorm:"index:idx_target_rater,priority:2;index"`
+	RaterID			string         `gorm:"index:idx_target_rater,priority:3;index"`
+	Score       	int            `gorm:"not null;check:score>=1 AND score<=5"`
+	Comment     	string         `gorm:"type:text"`
+	CreatedAt   	time.Time      `gorm:"autoCreateTime"`
+	UpdatedAt   	time.Time      `gorm:"autoUpdateTime"`
+}
