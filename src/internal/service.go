d9c001faccf238f745f16cdb114d76f3e1d71957
tince250 <t.s.mihajlovic25@gmail.com>
2025-10-28 14:38:16 +0100

    Added unit tests for create rating

--- src/internal/service.go
+++ src/internal/service.go
@@ -10,7 +10,7 @@ import (
 	"context"
 )
 
-// AuthContext is used in cases where callerID is not enoug
+// AuthContext is used in cases where callerID is not enough
 type AuthContext struct {
 	CallerID uint
 	JWT      string
